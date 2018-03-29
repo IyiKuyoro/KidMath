edecb0a6d9dab454ad3d2ff12d313bf7ba665fa0
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2018-03-29 13:05:39 +0100

    Games interface implemented

---
+++ KidMath/Game.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KidMath
+{
+    enum Operation
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
+    }
+    class Game
+    {
+        #region Fields
+        private readonly Settings settings;
+        private bool isCompleted;
+        private List<string> questions;
+        private List<int> givenAns;
+        private List<int> correctAns;
+        public List<Operation> operations;
+        private int score;
+        #endregion Fields
+
+        #region Properties
+        public Settings Settings
+        {
+            get { return settings; }
+        }
+        public bool IsCompleted
+        {
+            get { return isCompleted; }
+        }
+        public int Score
+        {
+            get
+            {
+                return score;
+            }
+        }
+        #endregion Properties
+
+        #region Methods
+        /// <summary>
+        /// Returns a string array of all the questions that was asked.
+        /// </summary>
+        /// <returns>A sting array of asked questions</returns>
+        public string[] GetQuestions()
+        {
+            return questions.ToArray();
+        }
+        /// <summary>
+        /// Returns an integer array of all the answers that where provided
+        /// </summary>
+        /// <returns>An array of integers</returns>
+        public int[] GetGivenAns()
+        {
+            return givenAns.ToArray();
+        }
+        /// <summary>
+        /// Returns an integer array of the correct answers to the questions asked
+        /// </summary>
+        /// <returns>An integer array or correct answers</returns>
+        public int[] GetCorrectAns()
+        {
+            return correctAns.ToArray();
+        }
+        /// <summary>
+        /// Saves a copy of the question asked into a string array
+        /// </summary>
+        /// <param name="firstOperand">The first operand in the question</param>
+        /// <param name="secondOperand">The second operand in the question</param>
+        /// <param name="operation">The operator</param>
+        public void SaveQuestion(int firstOperand, int secondOperand, Operation operation)
+        {
+            if (operation == Operation.Addition)
+            {
+                questions.Add(string.Format("{0} + {1}", firstOperand, secondOperand));
+                correctAns.Add(firstOperand + secondOperand);
+            }
+            else if (operation == Operation.Subtraction)
+            {
+                questions.Add(string.Format("{0} - {1}", firstOperand, secondOperand));
+                correctAns.Add(firstOperand - secondOperand);
+            }
+            else if (operation == Operation.Multiplication)
+            {
+                questions.Add(string.Format("{0} x {1}", firstOperand, secondOperand));
+                correctAns.Add(firstOperand * secondOperand);
+            }
+            else
+            {
+                questions.Add(string.Format("{0} / {1}", firstOperand, secondOperand));
+                correctAns.Add(firstOperand / secondOperand);
+            }
+        }
+        /// <summary>
+        /// Saves a copy of the answer the user provides for the question
+        /// </summary>
+        /// <param name="ans">The users answer</param>
+        public void SaveAns(int ans)
+        {
+            givenAns.Add(ans);
+        }
+        /// <summary>
+        /// Ends the game by computing the users score.
+        /// </summary>
+        private void EndGame()
+        {
+            isCompleted = true;
+            int correct = 0;
+            int i = 0;
+            foreach(int ans in givenAns)
+            {
+                if (ans == correctAns[i])
+                {
+                    correct++;
+                }
+                i++;
+            }
+
+            score = (correct / settings.Questions) * 100;
+        }
+        #endregion Methods
+
+        #region Constructor
+        public Game(Settings gameSettings)
+        {
+            isCompleted = false;
+            settings = gameSettings;
+            score = 0;
+            questions = new List<string>();
+            givenAns = new List<int>();
+            correctAns = new List<int>();
+            operations = new List<Operation>();
+
+            if(settings.WillAdd)
+                operations.Add(Operation.Addition);
+            if (settings.WillSubtract)
+                operations.Add(Operation.Subtraction);
+            if (settings.WillMultiply)
+                operations.Add(Operation.Multiplication);
+            if (settings.WillDivide)
+                operations.Add(Operation.Division);
+        }
+        #endregion Constructor
+    }
+}
--- KidMath/MainWindow.xaml.cs
+++ KidMath/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
+using System.Windows.Forms;
 
 namespace KidMath
 {
@@ -26,15 +27,34 @@ namespace KidMath
     public partial class MainWindow : Window
     {
         #region Fields
-        public Settings GameSettings;
+        public Settings gameSettings;
         private SerializableSettings serializableSettings;
+        private Game game;
+        private Timer gameTimer;
+        private int questionsCounter;
         #endregion Fields
+
+        #region Property
+        private int Clock
+        {
+            get
+            {
+                return (int)GetValue(ClockProperty);
+            }
+            set
+            {
+                SetValue(ClockProperty, value);
+            }
+        }
+
+        DependencyProperty ClockProperty = DependencyProperty.Register("Clock", typeof(int), typeof(MainWindow), new PropertyMetadata());
+        #endregion Property
 
         #region Methods
         /// <summary>
         /// Fades out and collapses all the buttons on the start screen.
         /// </summary>
-        private void FadeOutButtons()
+        private void FadeOutButtons(object sender, RoutedEventArgs e)
         {
             DoubleAnimation fadeAnimation = new DoubleAnimation
             {
@@ -42,20 +62,126 @@ namespace KidMath
                 To = 0,
                 Duration = TimeSpan.FromSeconds(1)
             };
-            fadeAnimation.Completed += CollapseStartPage;
 
-            cmdStartGame.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdSettings.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdHistory.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdAbout.BeginAnimation(Button.OpacityProperty, fadeAnimation);
+            //I am doing this because using one animation for all buttons causes threading issues down the line.
+            DoubleAnimation fadeAnimation1 = fadeAnimation.Clone();
+            DoubleAnimation fadeAnimation2 = fadeAnimation.Clone();
+            DoubleAnimation fadeAnimation3 = fadeAnimation.Clone();
+
+            fadeAnimation.Completed += CollapseStartPage;
+            if((System.Windows.Controls.Button)sender == cmdSettings)
+                fadeAnimation.Completed += ShowSettingsScreen;
+
+            if ((System.Windows.Controls.Button)sender == cmdStartGame)
+            {
+                fadeAnimation.Completed += ShowGameScreen;
+                fadeAnimation.Completed += StartGame;
+            }
+
+            cmdStartGame.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation);
+            cmdSettings.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation1);
+            cmdHistory.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation2);
+            cmdAbout.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation3);
         }
-        /// <summary>
-        /// Collapses all settings controls
-        /// </summary>
-        private void CollapseSettingsScreen()
+
+        private void StartGame(object sender, EventArgs e)
         {
-            settingsScreen.Visibility = Visibility.Collapsed;
+            //Control the timer
+            gameTimer = new Timer
+            {
+                Interval = 1000,
+                Enabled = true
+            };
+            gameTimer.Tick += ReduceTime;
+            gameTimer.Start();
+
+            this.answer.Focus();
+            messageBox.Text = "Let's play!";
+            GenerateQuestion();
         }
+
+        private void EndGame()
+        {
+            answer.IsEnabled = false;
+        }
+
+        /// <summary>
+        /// Randormly creates a question.
+        /// </summary>
+        private void GenerateQuestion()
+        {
+            if (questionsCounter == game.Settings.Questions)
+            {
+                EndGame();
+            }
+            else
+            {
+                try
+                {
+                    Random rnd = new Random();
+                    int firstOperand = rnd.Next(0, gameSettings.MaxOperand + 1);
+                    int secondOperand = rnd.Next(0, gameSettings.MaxOperand + 1);
+                    int opIndex = rnd.Next(0, game.operations.Count);
+
+                    //Generate the operation
+                    Operation operation = game.operations[opIndex];
+                    string operationText;
+
+                    //Display question
+                    switch (operation)
+                    {
+                        case (Operation.Addition):
+                            operationText = "+";
+                            @operator.Text = operationText;
+                            break;
+                        case (Operation.Subtraction):
+                            operationText = "-";
+                            @operator.Text = operationText;
+                            break;
+                        case (Operation.Multiplication):
+                            operationText = "x";
+                            @operator.Text = operationText;
+                            break;
+                        case (Operation.Division):
+                            operationText = "/";
+                            @operator.Text = operationText;
+                            break;
+                        default:
+                            throw new Exception("An error has occured generating the operation.");
+                    }
+                    txtFirstOperand.Text = firstOperand.ToString();
+                    txtSecondOperand.Text = secondOperand.ToString();
+
+                    game.SaveQuestion(firstOperand, secondOperand, operation);
+
+                    questionsCounter++;     //increase quesions count
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private void ReduceTime(object sender, EventArgs e)
+        {
+            if (Clock > 0)
+            {
+                Clock -= 1;
+
+                if (Clock == 5)
+                {
+                    timer.Foreground = new SolidColorBrush(Colors.Red);
+                    messageBox.Text = "Hurry Up!";
+                }
+            }
+            else
+            {
+                gameTimer.Stop();
+                EndGame();
+            }
+        }
+
         /// <summary>
         /// Fade in all the start screen controls.
         /// </summary>
@@ -75,18 +201,24 @@ namespace KidMath
             };
 
             //Apply fade animation to the controls
-            cmdStartGame.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdSettings.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdHistory.BeginAnimation(Button.OpacityProperty, fadeAnimation);
-            cmdAbout.BeginAnimation(Button.OpacityProperty, fadeAnimation);
+            cmdStartGame.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation);
+            cmdSettings.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation);
+            cmdHistory.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation);
+            cmdAbout.BeginAnimation(System.Windows.Controls.Button.OpacityProperty, fadeAnimation);
         }
+
         private void ReturnToStartScreen()
         {
             //Collapses settings screen and display start screen
-            CollapseSettingsScreen();
+            if(settingsScreen.Visibility == Visibility.Visible)
+                settingsScreen.Visibility = Visibility.Collapsed;
+            if (gameScreen.Visibility == Visibility.Visible)
+                gameScreen.Visibility = Visibility.Collapsed;
+
             container.Visibility = Visibility.Visible;
             ShowStartScreenControls();
         }
+
         private void ReadSettings()
         {
             BinaryFormatter formatter = new BinaryFormatter();
@@ -94,35 +226,35 @@ namespace KidMath
             {
                 serializableSettings = (SerializableSettings)formatter.Deserialize(fStream);
             }
-            GameSettings.Time = serializableSettings.time;
-            GameSettings.Questions = serializableSettings.questions;
-            GameSettings.MaxOperand = serializableSettings.maxOperand;
-            GameSettings.PlayerName = serializableSettings.playerName;
-            GameSettings.WillAdd = serializableSettings.add;
-            GameSettings.WillSubtract = serializableSettings.subtract;
-            GameSettings.WillMultiply = serializableSettings.multiply;
-            GameSettings.WillDivide = serializableSettings.divide;
+            gameSettings.Time = serializableSettings.time;
+            gameSettings.Questions = serializableSettings.questions;
+            gameSettings.MaxOperand = serializableSettings.maxOperand;
+            gameSettings.PlayerName = serializableSettings.playerName;
+            gameSettings.WillAdd = serializableSettings.add;
+            gameSettings.WillSubtract = serializableSettings.subtract;
+            gameSettings.WillMultiply = serializableSettings.multiply;
+            gameSettings.WillDivide = serializableSettings.divide;
         }
+
         private void WriteSettings()
         {
             //Save the settings data.
             BinaryFormatter formatter = new BinaryFormatter();
             using (Stream fStream = new FileStream("settings.dat", FileMode.Create, FileAccess.Write, FileShare.None))
             {
-                serializableSettings.time = GameSettings.Time;
-                serializableSettings.questions = GameSettings.Questions;
-                serializableSettings.maxOperand = GameSettings.MaxOperand;
-                serializableSettings.playerName = GameSettings.PlayerName;
-                serializableSettings.add = GameSettings.WillAdd;
-                serializableSettings.subtract = GameSettings.WillSubtract;
-                serializableSettings.multiply = GameSettings.WillMultiply;
-                serializableSettings.divide = GameSettings.WillDivide;
+                serializableSettings.time = gameSettings.Time;
+                serializableSettings.questions = gameSettings.Questions;
+                serializableSettings.maxOperand = gameSettings.MaxOperand;
+                serializableSettings.playerName = gameSettings.PlayerName;
+                serializableSettings.add = gameSettings.WillAdd;
+                serializableSettings.subtract = gameSettings.WillSubtract;
+                serializableSettings.multiply = gameSettings.WillMultiply;
+                serializableSettings.divide = gameSettings.WillDivide;
 
                 formatter.Serialize(fStream, serializableSettings);
             }
         }
 
-        #region EventHandlers
         /// <summary>
         /// Collapses all the buttons on the start screen.
         /// </summary>
@@ -131,12 +263,23 @@ namespace KidMath
         private void CollapseStartPage(object sender, EventArgs e)
         {
             container.Visibility = Visibility.Collapsed;
-            settingsScreen.Visibility = Visibility.Visible;
         }
+
         private void cmdSettings_Click(object sender, RoutedEventArgs e)
         {
-            FadeOutButtons();
+            FadeOutButtons(sender, e);
         }
+
+        private void ShowSettingsScreen(object sender, EventArgs e)
+        {
+            settingsScreen.Visibility = Visibility.Visible;
+        }
+
+        private void ShowGameScreen(object sender, EventArgs e)
+        {
+            gameScreen.Visibility = Visibility.Visible;
+        }
+
         /// <summary>
         /// Checks the box that was clicked and adjusts the settings.operationsList accordinly.
         /// </summary>
@@ -147,34 +290,74 @@ namespace KidMath
             ReadSettings();
             ReturnToStartScreen();
         }
+
         private void cmdSave_Click(object sender, RoutedEventArgs e)
         {
             WriteSettings();
             ReturnToStartScreen();
         }
+
         private void cmbDuration_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             cmbQuestions.Items.Clear();
 
             //Re-Populate Questions ComboBox
-            int curQs = GameSettings.MinQuestions;
-            while (curQs <= GameSettings.MaxQuestions)
+            int curQs = gameSettings.MinQuestions;
+            while (curQs <= gameSettings.MaxQuestions)
             {
                 cmbQuestions.Items.Add(string.Format(curQs + " Questions"));
                 curQs += 5;
             }
 
-            GameSettings.Questions = GameSettings.Time / 2;
+            gameSettings.Questions = gameSettings.Time / 2;
             cmbQuestions.SelectedItem = cmbQuestions.Items[0];
         }
-        #endregion EventHandlers
+
+        private void cmdStartGame_Click(object sender, RoutedEventArgs e)
+        {
+            game = new Game(gameSettings);
+            Clock = game.Settings.Time;     //initiates the timer to the game duration.
+            FadeOutButtons(sender, e);
+        }
+
+        private void answer_KeyUp(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                try
+                {
+                    game.SaveAns(Convert.ToInt32(this.answer.Text));
+                    this.answer.Focus();
+                    this.answer.Clear();
+                    if (questionsCounter != 1)
+                        messageBox.Text = string.Format("{0} questions to go!", game.Settings.Questions - questionsCounter);
+                    else
+                        messageBox.Text = string.Format("{0} question left.", game.Settings.Questions - questionsCounter);
+                    GenerateQuestion();
+                }
+                catch(FormatException)
+                {
+                    this.messageBox.Text = "Oops! That was not a number.";
+                    this.answer.Focus();
+                    this.answer.Clear();
+                }
+
+                e.Handled = true;
+            }
+        }
+
+        private void cmdEndGame_Click(object sender, RoutedEventArgs e)
+        {
+            EndGame();
+        }
         #endregion Methods
 
         #region Contructors
         public MainWindow()
         {
             InitializeComponent();
-            GameSettings = new Settings();
+            gameSettings = new Settings();
+            questionsCounter = 0;
 
             //Map GameSettings class to an instance of SerializableSettings
             serializableSettings = new SerializableSettings();
@@ -188,7 +371,7 @@ namespace KidMath
                 WriteSettings();
             }
 
-            DataContext = GameSettings;
+            DataContext = gameSettings;
 
             //Populate Duration ComboBox
             cmbDuration.ItemsSource = new string[] { "10 Seconds", "20 Seconds", "30 Seconds", "40 Seconds", "50 Seconds", "60 Seconds" };
